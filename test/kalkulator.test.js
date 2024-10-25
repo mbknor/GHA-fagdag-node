4daf593e44f8f5614aaabea3dfbd0d0d185503db
Morten Kjetland <mbk@kjetland.com>
2024-10-25 13:14:46 +0200

    o 5 fixing test

--- test/kalkulator.test.js
+++ test/kalkulator.test.js
@@ -2,5 +2,5 @@ const { expect, test } = require("@jest/globals");
 const sum = require("../src/kalkulator");
 
 test("1 + 2 er 3", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(1 + 2).toBe(3);
 });
